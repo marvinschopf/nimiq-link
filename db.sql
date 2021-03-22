d51dc5553678515573fa386b19129a8cf68228fa
Marvin Schopf <marvin@schopf.biz>
2021-03-22 02:58:04 +0100

    Fix database structure

--- db.sql
+++ db.sql
@@ -19,5 +19,5 @@
 CREATE TABLE `links` ( `id` VARCHAR(255) NOT NULL , `destination` TEXT NOT NULL , `domain` VARCHAR(255) NOT NULL , `slug` VARCHAR(255) NOT NULL , `active` INT(1) NOT NULL DEFAULT '1' , `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , `adminpassword` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 ALTER TABLE `links` ADD `locked` INT(1) NOT NULL DEFAULT '0' AFTER `adminpassword`;
 ALTER TABLE `links` ADD `lockReason` VARCHAR(255) NULL AFTER `adminpassword`;
-CREATE TABLE `clicks` ( `id` INT NOT NULL AUTO_INCREMENT , `link` VARCHAR(255) NOT NULL , `date` DATE NOT NULL , `clicks` INT NOT NULL DEFAULT '1' , PRIMARY KEY (`id`), UNIQUE `link` (`link`), UNIQUE `date` (`date`)) ENGINE = InnoDB;
+CREATE TABLE `clicks` ( `id` INT NOT NULL AUTO_INCREMENT , `link` VARCHAR(255) NOT NULL , `date` DATE NOT NULL , `clicks` INT NOT NULL DEFAULT '1' , PRIMARY KEY (`id`, `link`), UNIQUE `date` (`date`)) ENGINE = InnoDB;
 ALTER TABLE `clicks` ADD FOREIGN KEY (`link`) REFERENCES `links`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
